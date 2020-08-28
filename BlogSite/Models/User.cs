eeb8f26c1e49f5f3ef221d741e2e1e93663d50ef
autumn-ragland <raglanda998@gmail.com>
2020-08-28 11:42:05 -0500

    update LName to lName and add postman

--- BlogSite/Models/User.cs
+++ BlogSite/Models/User.cs
@@ -4,16 +4,16 @@ namespace BlogSite.Models
     public class User
     {
         public string fName;
-        public string LName;
+        public string lName;
         public string userName;
         public string bio;
         public bool highlighted;
         public int userID;
         public List<Blog> blogs = new List<Blog>();
-        public User(string fName, string LName, string userName, string bio, List<Blog> blogs, bool highlighted, int userID)
+        public User(string fName, string lName, string userName, string bio, List<Blog> blogs, bool highlighted, int userID)
         {
             this.fName = fName;
-            this.LName = LName;
+            this.lName = lName;
             this.userName = userName;
             this.bio = bio;
             this.blogs = blogs;
